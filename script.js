72a0365814c78f813bc52b2d14132f6a2bbbfcac
Szymsi <sienkiewicz.szymon@gmail.com>
2016-11-27 16:57:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ var triangle1Area = getTriangleArea(10, 15);
 var triangle2Area = getTriangleArea(3, 5);
 var triangle3Area = getTriangleArea(4, 9);
 
-function getTriangleArea (a,h) {
+function getTriangleArea (a, h) {
+
   if (a < 0 &&  h < 0) {
-  return console.log ("Nieprawidłowa wartość");
+    return console.log ("Nieprawidłowa wartość");
   }
-  else {
-  return a*h/2;
+  return a * h / 2;
   }
 }
 
